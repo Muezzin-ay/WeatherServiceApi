2b70a49e6547dae85ae0904d596613c5d5d9eb21
Robin Hart <robinhrt05@gmail.com>
2020-07-28 19:59:31 +0200

    added the droppdown to navbar

---
+++ public/src/components/currentTime.js
@@ -0,0 +1,20 @@
+Vue.component("current-time",{
+    props : [],
+    data : ()=>{
+        return{
+
+        }
+    },
+    methods : {
+    },
+    computed : {
+
+    },
+    mounted : function(){
+
+    },
+    template : 
+    `
+    <h1>current-time</h1>
+    `
+})
